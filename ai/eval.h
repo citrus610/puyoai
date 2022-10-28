0ffc036d318decd589ff36a71cb2d0002da84e06
citrus610 <79373642+citrus610@users.noreply.github.com>
2022-10-28 16:58:30 +0700

    update new eval

--- ai/eval.h
+++ ai/eval.h
@@ -31,17 +31,17 @@ public:
 };
 
 constexpr Weight DEFAULT_WEIGHT = {
-    .link_ver = 10,
+    .link_ver = 15,
     .link_hor = 15,
     .d_height = -5,
     .d_height_sq = -1,
     .symm = -5,
-    .symm_sq = -1,
-    .shape_u = -5,
-    .shape_u_sq = -1,
+    .symm_sq = -2,
+    .shape_u = -10,
+    .shape_u_sq = -10,
 
-    .ptnl_chain_count = 100,
-    .ptnl_chain_score = 3,
-    .ptnl_chain_added = -50,
-    .ptnl_chain_height = 150,
+    .ptnl_chain_count = 75,
+    .ptnl_chain_score = 1,
+    .ptnl_chain_added = -100,
+    .ptnl_chain_height = 100,
 };
